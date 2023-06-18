eb6fec57c8bc9d2441d0f63e47a942dd0d2bea4b
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2023-06-18 17:54:16 -0300

    start with variables

---
+++ fundamentals/1_variables/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        int passengers = 5;
+        System.out.println(passengers);
+
+        int busTickets = passengers;
+        System.out.println(busTickets);
+
+        passengers += 9;
+        System.out.println(passengers);
+
+        passengers -= 3;
+        System.out.println(passengers);
+    }
+}
