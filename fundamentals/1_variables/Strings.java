2404c6b58635350b89363852e4bfd4271da63dfc
Gabriel Gradinar <gabriel.gradinar@open-co.com>
2023-06-19 17:41:19 -0300

    Strings and chars

---
+++ fundamentals/1_variables/Strings.java
@@ -0,0 +1,21 @@
+public class Strings {
+
+    public static void main(String[] args) {
+        String name = "Gabriel";
+        int age = 25;
+
+        String text = "Hi I'm " + name + " and I " + age + " years old !";
+
+        System.out.println(text);
+
+        char potions = 'A';
+        char arithmancy = 'B';
+        char chams = 'C';
+
+        System.out.println("Potions " + potions);
+        System.out.println("Arithmancy " + arithmancy);
+        System.out.println("Chams " + chams);
+
+    }
+
+}
