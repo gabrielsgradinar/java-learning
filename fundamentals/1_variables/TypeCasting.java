b10f6238b3eb282f3bfcccbe124fb24fae3610f8
Gabriel Gradinar <gabriel.gradinar@open-co.com>
2023-06-22 10:37:21 -0300

    type cast and use scanner

---
+++ fundamentals/1_variables/TypeCasting.java
@@ -0,0 +1,14 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        int ants = 2000;
+        int purchasedBlocksOfCheese = 1;
+
+        System.out.println(((double) purchasedBlocksOfCheese / ants));
+
+        double salary = 5398.55;
+        int roundedSalary = (int) salary;
+        System.out.println(salary);
+        System.out.println(roundedSalary);
+
+    }
+}
