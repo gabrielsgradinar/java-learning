e58443f95b6018cf2e9151200e11ff774000dacc
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2023-06-22 17:58:28 -0300

    more functions

--- fundamentals/3_functions/Functions.java
+++ fundamentals/3_functions/Functions.java
@@ -4,11 +4,31 @@ public class Functions {
 
         System.out.println(calculatedArea);
 
+        String explanation = explainArea("French");
+        System.out.println(explanation);
+
     }
 
     public static double calculateArea(double lenght, double width) {
-        double area = lenght * width;
+        if (lenght < 0 || width < 0) {
+            System.out.println("Invalid Dimensions");
+            System.exit(0);
+        }
+        return lenght * width;
+    }
 
-        return area;
+    public static String explainArea(String language) {
+        switch (language) {
+            case "English":
+                return "Area equals length * width";
+            case "French":
+                return "La surface est egale a la longueur * la largeur";
+            case "Spanish":
+                return "area es igual a largo * ancho";
+            default:
+                return "Language not available";
+        }
     }
 }
+
+// Spanish "area es igual a largo * ancho"
