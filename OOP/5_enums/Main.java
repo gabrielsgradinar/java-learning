8ad12c54390c8b9764e84edf8135ac1d4a3b2aaa
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2023-07-14 11:32:11 -0300

    enums

---
+++ OOP/5_enums/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        enum TrafficLight {
+            RED, GREEN, YELLOW
+        }
+
+        var red = TrafficLight.RED;
+
+        System.out.println(red);
+
+    }
+}
